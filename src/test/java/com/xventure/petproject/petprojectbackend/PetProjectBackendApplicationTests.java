6ea115f242d4980c36f37773ce88cbfca59d749d
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-11-02 13:16:35 +0530

    first commit

---
+++ src/test/java/com/xventure/petproject/petprojectbackend/PetProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xventure.petproject.petprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
