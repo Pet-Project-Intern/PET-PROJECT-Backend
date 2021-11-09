bdb112e9e2713d1166b1e2d3c4fb6d8933f97a83
DELL <isurumaldeiya@gmail.com>
2021-11-09 15:23:49 +0530

    user login is completed manually

--- src/main/java/com/xventure/petproject/petprojectbackend/controller/UserController.java
+++ src/main/java/com/xventure/petproject/petprojectbackend/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.xventure.petproject.petprojectbackend.controller;
 
+import com.xventure.petproject.petprojectbackend.dto.UserDTO;
 import com.xventure.petproject.petprojectbackend.entity.User;
 import com.xventure.petproject.petprojectbackend.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -63,8 +64,29 @@ public class UserController {
     }
 
 
-
-
+    @GetMapping(value = "/loginUser")
+    @Operation(summary = "login validation")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "User is Exist",
+                    content = @Content(mediaType = "application/json")
+            ),
+            @ApiResponse(responseCode = "400",
+                    description = "Email address is incorrect",
+                    content = @Content(mediaType = "application/json")
+            ),
+            @ApiResponse(responseCode = "401",
+                    description = "You are not registered in the system. Please register in the system",
+                    content = @Content(mediaType = "application/json")
+            ),
+            @ApiResponse(responseCode = "402",
+                    description = "Password is incorrect. Please check and try again",
+                    content = @Content(mediaType = "application/json")
+            )
+    })
+    public ResponseEntity<UserDTO> login(@RequestBody UserDTO dto){
+        return userService.loginUser(dto.getEmailId(), dto.getPassword());
+    }
 
 
 }
--- src/main/java/com/xventure/petproject/petprojectbackend/service/UserService.java
+++ src/main/java/com/xventure/petproject/petprojectbackend/service/UserService.java
@@ -1,5 +1,6 @@
 package com.xventure.petproject.petprojectbackend.service;
 
+import com.xventure.petproject.petprojectbackend.dto.UserDTO;
 import com.xventure.petproject.petprojectbackend.entity.User;
 import com.xventure.petproject.petprojectbackend.repositry.UserRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -8,7 +9,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Service
 @Slf4j
@@ -28,7 +33,7 @@ public class UserService {
         log.info("Inside addUser method in UserService");
         userRepository.save(user);
 
-        return ResponseEntity.ok("User added Succssfully");
+        return ResponseEntity.ok("User added Successfully");
     }
 
     //check the email does exist
@@ -42,5 +47,58 @@ public class UserService {
       return ResponseEntity.ok(true);
 
 
+    }
+
+//    hashing method
+    private String hashingPass(String text){
+        MessageDigest digest = null;
+        String s=null;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+            s=digest.digest(text.getBytes(StandardCharsets.UTF_8)).toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return s;
+    }
+
+    private String removeWhiteSpaces(String s){
+        return s.replaceAll("\\s+","");
+    }
+
+
+    private boolean isEmailSyntactical(String mail){
+        Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile(
+                "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
+                Pattern.CASE_INSENSITIVE
+        );
+        return VALID_EMAIL_ADDRESS_REGEX.matcher(mail).find();
+    }
+
+    public ResponseEntity<UserDTO> loginUser(String email, String password){
+
+//        removing white spaces
+        String mail=removeWhiteSpaces(email);
+        String pass=removeWhiteSpaces(password);
+//        mail syntax checking
+        if(!isEmailSyntactical(mail)){
+            return ResponseEntity.status(400).body(null);
+        }
+//        user loading
+        User search = userRepository.findByEmailId(mail);
+//        if there is no user
+        if(search==null){
+            return ResponseEntity.status(401).body(null);
+        }
+//        if password mismatching
+        if(!search.getPassword().equals(pass)){
+            return ResponseEntity.status(402).body(null);
+        }
+//        if all is well
+        return ResponseEntity.status(200).body(new UserDTO(
+                search.getId(),
+                search.getEmailId(),
+                search.getPassword()
+        ));
     }
 }
